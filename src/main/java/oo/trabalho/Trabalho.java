40073b0260ebd31478fa6f758c20b2b0a713c8e6
pdetoni <pdpereira2003@gmail.com>
2023-12-04 22:23:33 -0300

    teste de clone e push

--- src/main/java/oo/trabalho/Trabalho.java
+++ src/main/java/oo/trabalho/Trabalho.java
@@ -11,6 +11,6 @@ package oo.trabalho;
 public class Trabalho {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Trabalho OO");
     }
 }
